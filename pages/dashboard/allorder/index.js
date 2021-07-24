f03f1919553307266e9a423f72669448146be400
nrd141913 <nrd141913@.com>
2021-07-24 14:32:12 +0600

    ok2

--- pages/dashboard/allorder/index.js
+++ pages/dashboard/allorder/index.js
@@ -1,31 +1,46 @@
-import React from 'react';
-import Allorder from '../../../components/Dashboard/Allorder/Allorder';
+import React from "react";
+import Allorder from "../../../components/Dashboard/Allorder/Allorder";
 
 const index = (props) => {
-    console.log(props)
-    return (
-        <div>
-            <Allorder props={props} />
-        </div>
-    );
+  console.log(props);
+  return (
+    <div>
+      <Allorder props={props} />
+    </div>
+  );
 };
 
-export async function getStaticProps() {
-    const res = await fetch('https://jsonplaceholder.typicode.com/posts')
-    const data = await res.json()
-    
-  
-    if (!data) {
-      return {
-        notFound: true,
-      }
-    }
-  
+// export async function getStaticProps() {
+//   const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+//   const data = await res.json();
+
+//   if (!data) {
+//     return {
+//       notFound: true,
+//     };
+//   }
+
+//   return {
+//     props: {
+//       data,
+//     }, // will be passed to the page component as props
+//   };
+// }
+export async function getServerSideProps() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await res.json();
+
+  if (!data) {
     return {
-      props: { 
-        data,
-      }, // will be passed to the page component as props
-    }
+      notFound: true,
+    };
   }
-  
+
+  return {
+    props: {
+      data,
+    }, // will be passed to the page component as props
+  };
+}
+
 export default index;
